f2f5a68a47f277233504c99974589655e65c7541
Kore Nordmann <mail@kore-nordmann.de>
2014-04-21 18:56:32 +0200

    Installed angular components

--- src/require.config.js
+++ src/require.config.js
@@ -1,11 +1,22 @@
 /* global require */
 require.config({
     "basePath": './',
-
     "paths": {
-
+        "angular": "../bower_components/angular/angular",
+        "angular-route": "../bower_components/angular-route/angular-route",
+        "angular-ui": "../bower_components/angular-ui/build/angular-ui",
+        "domReady": "../bower_components/requirejs-domready/domReady"
     },
     "shim": {
-
-    }
+        "angular": {
+            exports: "angular"
+        },
+        "angular-route": {
+            exports: "angular-route"
+        },
+        "angular-ui": {
+            exports: "angular-ui"
+        }
+    },
+    deps: ['./bootstrap']
 });
