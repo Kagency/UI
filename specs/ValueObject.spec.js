61cdd740b216996c5746daaffc4f83b84e7374b4
Kore Nordmann <mail@kore-nordmann.de>
2014-12-13 12:18:46 +0100

    Tested setting / getting unknown properties

--- specs/ValueObject.spec.js
+++ specs/ValueObject.spec.js
@@ -1,23 +1,45 @@
 define([
     'ValueObject'
 ], function (ValueObject) {
+
+    var TestObject = function () {
+        this.property = null;
+
+        ValueObject.apply(this, arguments);
+    };
+    TestObject.prototype = ValueObject.prototype;
+
     describe("ValueObject", function() {
         it("returns set value on get", function() {
-            var valueObject = new ValueObject();
+            var valueObject = new TestObject();
             valueObject = valueObject.set("property", "value");
             expect(valueObject.get("property")).toEqual("value");
         });
 
+        it("fails on get on unknown property", function() {
+            var valueObject = new TestObject();
+            expect(function () {
+                valueObject.get("unknown");}
+            ).toThrow(new Error("Property unknown is undefined."));
+        });
+
+        it("fails on set on unknown property", function() {
+            var valueObject = new TestObject();
+            expect(function () {
+                valueObject.set("unknown");}
+            ).toThrow(new Error("Property unknown is undefined."));
+        });
+
         it("does not change original value on changed property", function() {
-            var original = new ValueObject(),
+            var original = new TestObject(),
                 copy = original.set("property", "value");
 
-            expect(original.property).toBeUndefined();
+            expect(original.property).toBeNull();
             expect(copy.get("property")).toEqual("value");
         });
 
         it("does not change original value on changed property", function() {
-            var original = new ValueObject(),
+            var original = new TestObject(),
                 copy;
 
             original = original.set("property", "value");
@@ -28,11 +50,19 @@ define([
         });
 
         it("init sets all passed properties", function() {
-            var valueObject = new ValueObject({
+            var valueObject = new TestObject({
                     property: "value"
                 });
 
             expect(valueObject.get("property")).toEqual("value");
+        });
+
+        it("fails on init on unknown property", function() {
+            expect(function () {
+                var valueObject = new TestObject({
+                    unknown: "value"
+                });
+            }).toThrow(new Error("Property unknown is undefined."));
         });
     });
 });
--- src/ValueObject.js
+++ src/ValueObject.js
@@ -6,13 +6,23 @@ define([
         var properties = properties || {};
 
         for (var property in properties) {
-            if (properties.hasOwnProperty(property)) {
-                this[property] = properties[property];
+            if (!properties.hasOwnProperty(property)) {
+                continue;
             }
+
+            if (this[property] === undefined) {
+                throw "Property " + property + " is undefined.";
+            }
+
+            this[property] = properties[property];
         }
     };
 
     ValueObject.prototype.set = function(property, value) {
+        if (this[property] === undefined) {
+            throw "Property " + property + " is undefined.";
+        }
+
         var newValue = Object.create(this);
         newValue[property] = value;
         return newValue;
