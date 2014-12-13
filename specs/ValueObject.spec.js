ef42be9fd09ee8cb1403b5fe4fe36cfc2150d690
Kore Nordmann <mail@kore-nordmann.de>
2014-12-13 10:05:48 +0100

    Fixed test name

--- specs/ValueObject.spec.js
+++ specs/ValueObject.spec.js
@@ -2,7 +2,7 @@ define([
     'ValueObject'
 ], function (ValueObject) {
     describe("ValueObject", function() {
-        it("Get returns set value", function() {
+        it("returns set value on get", function() {
             var valueObject = new ValueObject();
             valueObject = valueObject.set("property", "value");
             expect(valueObject.get("property")).toEqual("value");
