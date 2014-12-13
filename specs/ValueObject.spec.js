36f98e9984b4dd20b4ee7ac3f8faec51cc0d18fd
Kore Nordmann <mail@kore-nordmann.de>
2014-12-13 10:54:30 +0100

    Make sure copy on write works

--- specs/ValueObject.spec.js
+++ specs/ValueObject.spec.js
@@ -7,5 +7,21 @@ define([
             valueObject = valueObject.set("property", "value");
             expect(valueObject.get("property")).toEqual("value");
         });
+
+        it("does not change original value on changed property", function() {
+            var original = new ValueObject(),
+                copy = original.set("property", "value");
+
+            expect(original.property).toBeUndefined();
+        });
+
+        it("does not change original value on changed property", function() {
+            var original = new ValueObject(),
+                original = original.set("property", "value"),
+                copy = original.set("property", "new value");
+
+            expect(original.get("property")).toEqual("value");
+            expect(copy.get("property")).toEqual("new value");
+        });
     });
 });
