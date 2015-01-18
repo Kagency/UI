e55bbb8ce9af9bfe22dd895594ac01ae65de51a1
Kore Nordmann <mail@kore-nordmann.de>
2015-01-18 12:30:38 +0100

    Configured PouchDB (one-time) sync

--- src/Storage.js
+++ src/Storage.js
@@ -6,7 +6,18 @@ define([
     var Storage = function () {
     };
 
-    Storage.database = new PouchDB("storage");
+    Storage.database = new PouchDB("api");
+    Storage.remote = new PouchDB(
+        window.location.protocol + "//" +
+        window.location.hostname + ":" +
+        window.location.port + "/api"
+    );
+
+    Storage.database.sync(Storage.remote).on("complete", function () {
+        console.info("In sync");
+    }).on("error", function(error) {
+        alert("Error: " + error);
+    });
 
     return Storage;
 });
